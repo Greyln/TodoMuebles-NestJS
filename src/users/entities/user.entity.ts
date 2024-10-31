b3a146b45515f4cfce180199c684a53286fddcf1
Lucyy_ <138951685+Greyln@users.noreply.github.com>
2024-10-31 19:50:03 -0300

    Update user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, Timestamp } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    displayname: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    created_at: Timestamp;
+
+    @Column({ default: true })
+    isActive: boolean;
+}
